9ceb74cfdfa632eaa189392cf6a35c5574843999
Kirill Opekishev <grumpykir@gmail.com>
2019-04-12 11:29:04 +0300

    Initial Commit

---
+++ GKRepresentable/GKRepresentable.h
@@ -0,0 +1,19 @@
+//
+//  GKRepresentable.h
+//  GKRepresentable
+//
+//  Created by  Кирилл on 4/12/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKRepresentable.
+FOUNDATION_EXPORT double GKRepresentableVersionNumber;
+
+//! Project version string for GKRepresentable.
+FOUNDATION_EXPORT const unsigned char GKRepresentableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKRepresentable/PublicHeader.h>
+
+
